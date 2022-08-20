279e01fb718829ed9204e244c0737b03bcb737b0
Micael Jerry <hei.jerry.2@gmail.com>
2022-08-20 05:25:03 +0300

    init 2nd prep hackathon

---
+++ src/main/java/com/example/helloworldapi/CocktailBarApi.java
@@ -0,0 +1,13 @@
+package com.example.helloworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailBarApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailBarApi.class, args);
+    }
+
+}
